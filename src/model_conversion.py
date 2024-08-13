8a6c7c8233873aab1f3ee22f5c4fbdb69cf94f2f
etueschenboe <eric.tueschenboenner@gmail.com>
2024-08-13 11:18:26 +0200

    added scene coordinate conversion

--- src/model_conversion.py
+++ src/model_conversion.py
@@ -47,28 +47,33 @@ def convert_matrix_to_pose(T: np.ndarray) -> np.ndarray:
 class ModelConversion:
     """
     Convert between formats of CAD and SFM (COLMAP) models.
+    - transform poses between CAD and COLMAP formats.
+    - convert depth maps from EXR to NPZ format.
+    - visualize depth maps.
     """
     def __init__(
             self,
             path_to_ground_truth: Path,
-            path_to_database: Path,
+            path_to_database: Path = None,
             images_prefix: str = 'images/',
             poses_prefix: str = 'poses/',
             depth_prefix: str = 'depth/',
             ):
         
         self.path_to_ground_truth = path_to_ground_truth
-        self.T_cad_sfm = self.read_registration_matrix(path_to_ground_truth / 'T_cad_sfm.txt')
+        self.T_cad_sfm, self.s_cad_sfm = self.read_registration_data(path_to_ground_truth / 'T_cad_sfm.txt')
         self.T_sfm_cad = np.linalg.inv(self.T_cad_sfm)
 
         self.path_to_ground_truth = path_to_ground_truth
-        self.path_to_database = path_to_database
 
-        self.path_to_poses = path_to_database / poses_prefix
-        self.path_to_images = path_to_database / images_prefix
-        self.path_to_depth = path_to_database / depth_prefix
+        if path_to_database:
+            self.path_to_database = path_to_database
 
-        self.image_names = self.read_image_names(self.path_to_images)
+            self.path_to_poses = path_to_database / poses_prefix
+            self.path_to_images = path_to_database / images_prefix
+            self.path_to_depth = path_to_database / depth_prefix
+
+            self.image_names = self.read_image_names(self.path_to_images)
         
     @staticmethod
     def read_image_names(file_path) -> List[str]:
@@ -77,18 +82,18 @@ class ModelConversion:
         return image_names
 
     @staticmethod
-    def read_registration_matrix(file_path: Path) -> np.ndarray:
+    def read_registration_data(file_path: Path) -> np.ndarray:
         T_cad_sfm = np.loadtxt(file_path)
         assert T_cad_sfm.shape == (4,4), T_cad_sfm.shape
 
         scale, shear, angles, translate, perspective = decompose_matrix(T_cad_sfm)
         q_cad_sfm = Quaternion(matrix=Rotation.from_euler('xyz', angles).as_matrix()).inverse
         pose_cad_sfm = np.hstack([translate, q_cad_sfm.q])
-        s_cad_sfm = sum(scale) / len(scale)
+        s_cad_sfm = np.average(scale)
         print('SFM pose (CAD frame): ', pose_cad_sfm)
         print('SFM vs. CAD scale: ', s_cad_sfm)
 
-        return T_cad_sfm
+        return T_cad_sfm, s_cad_sfm
     
 
     """
@@ -139,9 +144,27 @@ class ModelConversion:
         T_cad_cam = compose_matrix(None, None, angles, translate)
         pose_cad_cam = convert_matrix_to_pose(T_cad_cam)
 
-        pose_cad_cam[:3] = pose_cad_cam[:3] / scale
+        # pose_cad_cam[:3] = pose_cad_cam[:3] / scale
 
         return pose_cad_cam
+    
+    def transform_poses_from_colmap_to_cad_format(
+            self,
+            poses_cam_sfm: Dict[str, np.ndarray],
+            to_blender_format: bool,
+            ) -> Dict[str, np.ndarray]:
+        """
+        Transform poses in COLMAP to CAD format.
+        Optional: reverse poses to Blender format (camera facing -z direction).
+        """
+        poses_cad_cam = {}
+        for query_name, pose_cam_sfm in poses_cam_sfm.items():
+            pose_cad_cam = self.transform_pose_from_colmap_to_cad_format(
+                pose_cam_sfm=pose_cam_sfm,
+                to_blender_format=to_blender_format,
+            )
+            poses_cad_cam[query_name] = pose_cad_cam
+        return poses_cad_cam
     
     def transform_pose_from_cad_to_colmap_format(
             self,
@@ -172,6 +195,24 @@ class ModelConversion:
         pose_cam_sfm[:3] = pose_cam_sfm[:3] / scale
 
         return pose_cam_sfm
+    
+    def transform_poses_from_cad_to_colmap_format(
+            self,
+            poses_cad_cam: Dict[str, np.ndarray],
+            from_blender_format: bool,
+            ) -> Dict[str, np.ndarray]:
+        """
+        Transform poses in CAD to COLMAP format.
+        Optional: reverse camera poses from Blender format (facing -z direction).
+        """
+        poses_cam_sfm = {}
+        for query_name, pose_cad_cam in poses_cad_cam.items():
+            pose_cam_sfm = self.transform_pose_from_cad_to_colmap_format(
+                pose_cad_cam=pose_cad_cam,
+                from_blender_format=from_blender_format,
+            )
+            poses_cam_sfm[query_name] = pose_cam_sfm
+        return poses_cam_sfm
 
 
     """
@@ -237,28 +278,28 @@ class ModelConversion:
         write_images_binary(images, self.path_to_database / 'images.bin')
         write_images_text(images, self.path_to_database / 'images.txt')
 
-        points3d = {}
-        # Read bounding box coordinates from database
-        with open(self.path_to_database / 'bounding_box.txt', 'r') as file:
-            lines = file.readlines()
-            for i, line in enumerate(lines):
-                line.strip()
-                xyz_cad = np.array([float(x) for x in line.split()])
-                xyz_sfm = (self.T_sfm_cad @ np.append(xyz_cad, 1))[:3]
+        # points3d = {}
+        # # Read bounding box coordinates from database
+        # with open(self.path_to_database / 'bounding_box.txt', 'r') as file:
+        #     lines = file.readlines()
+        #     for i, line in enumerate(lines):
+        #         line.strip()
+        #         xyz_cad = np.array([float(x) for x in line.split()])
+        #         xyz_sfm = (self.T_sfm_cad @ np.append(xyz_cad, 1))[:3]
 
-                print(f'Point {i+1}: {xyz_cad} (CAD) -> {xyz_sfm} (SFM)')
+        #         print(f'Point {i+1}: {xyz_cad} (CAD) -> {xyz_sfm} (SFM)')
 
-                points3d[i+1] = Point3D(
-                    id=i+1,
-                    xyz=xyz_sfm,
-                    rgb=np.array([0, 0, 0]),
-                    error=0,
-                    image_ids=np.array([]),
-                    point2D_idxs=np.array([]),
-                )
+        #         points3d[i+1] = Point3D(
+        #             id=i+1,
+        #             xyz=xyz_sfm,
+        #             rgb=np.array([0, 0, 0]),
+        #             error=0,
+        #             image_ids=np.array([]),
+        #             point2D_idxs=np.array([]),
+        #         )
 
-        write_points3D_binary(points3d, self.path_to_database / 'points3D.bin')
-        write_points3D_text(points3d, self.path_to_database / 'points3D.txt')
+        # write_points3D_binary(points3d, self.path_to_database / 'points3D.bin')
+        # write_points3D_text(points3d, self.path_to_database / 'points3D.txt')
 
 
 
@@ -267,7 +308,7 @@ class ModelConversion:
     """
 
     @staticmethod
-    def convert_depth_map_from_exr_to_npz(path_to_depth: Path, name: str):
+    def convert_depth_map_from_exr_to_npz(path_to_depth: Path, name: str, scale: float = None):
         """
         Convert EXR depth maps to NPZ format.
         """
@@ -290,6 +331,10 @@ class ModelConversion:
         # Set background to zero
         depth = np.where(depth < 1000, depth, 0)
 
+        # scale depth values
+        if scale:
+            depth = depth / scale
+
         depth_values_dict = {}
         depth_values_dict['depth'] = depth        
         np.savez_compressed(path_to_depth / (name + '.npz'), **depth_values_dict)
@@ -299,14 +344,17 @@ class ModelConversion:
         Convert all EXR depth maps to NPZ format.
         """
         for image_name in self.image_names:
-            self.convert_depth_map_from_exr_to_npz(self.path_to_depth, image_name)
+            self.convert_depth_map_from_exr_to_npz(self.path_to_depth, image_name, scale=self.s_cad_sfm)
 
     @staticmethod
-    def visualize_depth_map(path_to_depth: Path, name: str, out: str = 'show'):
+    def visualize_depth_map(path_to_depth: Path, name: str, path_to_output: Path = None):
         """
         Visualize npy/npz depth map with colors according to depth values and a legend.
         Goal: to check if depth map is correct.
         """
+
+        cmap = plt.get_cmap('viridis')
+        cmap.set_bad(color='white')
 
         name = name.split('.')[0]
         if not name.endswith('_depth'):
@@ -314,10 +362,6 @@ class ModelConversion:
         depth_name = name + '.npz'
 
         depth_map = np.load(path_to_depth / depth_name)['depth']
-
-        # Create a custom colormap with white for zero values
-        cmap = plt.get_cmap('viridis').copy()
-        cmap.set_bad(color='white')
         
         # Create a masked array, masking zero values
         masked_depth_map = np.ma.masked_where(depth_map == 0, depth_map)
@@ -325,7 +369,7 @@ class ModelConversion:
         fig, ax = plt.subplots(figsize=(10, 8))
 
         # Create a color-coded image of the depth map
-        im = ax.imshow(masked_depth_map, cmap='viridis')
+        im = ax.imshow(masked_depth_map, cmap=cmap)
 
         # Reverse the y-axis
         # ax.invert_yaxis()
@@ -334,22 +378,154 @@ class ModelConversion:
         cbar = fig.colorbar(im, ax=ax)
         cbar.set_label('Depth', rotation=270, labelpad=15)
         
-        # Set title and labels
-        ax.set_title('Depth Map Visualization')
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
+        # ax.set_title('Depth Map Visualization')
+
+        # ax.set_xlabel('X')
+        # ax.set_ylabel('Y')
+        # ax.axis('off')
+
+        ax.set_xticks([])
+        ax.set_yticks([])
         
-        if out == 'show':
+        if not path_to_output:
             plt.show()
-        elif out == 'save':
-            plt.savefig(path_to_depth / f'{name}_depth.png')
-            plt.close()
         else:
-            raise ValueError(f"Invalid output option: {out}")
+            plt.savefig(path_to_output / f'{name}.png', transparent=True)
+        plt.close()
+
         
-    def visualize_depth_maps(self):
+    # def visualize_depth_maps(self):
+    #     """
+    #     Create depth images from depth maps and save them.
+    #     """
+    #     for image_name in self.image_names:
+    #         self.visualize_depth_map(self.path_to_depth, image_name, out='save')
+
+
+    """
+    SCENE COORDINATES:
+    """
+
+    @staticmethod
+    def convert_depth_map_to_scene_coordinate_map(
+            depth_map: np.ndarray,
+            camera_intrinsics: np.ndarray,
+            pose_cam_sfm: np.ndarray,
+        ) -> np.ndarray:
         """
-        Create depth images from depth maps and save them.
+        Convert depth map to scene coordinates.
         """
-        for image_name in self.image_names:
-            self.visualize_depth_map(self.path_to_depth, image_name, out='save')
+        T_cam_sfm = convert_pose_to_matrix(pose_cam_sfm)
+        T_sfm_cam = np.linalg.inv(T_cam_sfm)
+
+        fx, fy, cx, cy = camera_intrinsics
+
+        x, y = np.meshgrid(np.arange(depth_map.shape[1]), np.arange(depth_map.shape[0]))
+        x = (x - cx) * depth_map / fx
+        y = (y - cy) * depth_map / fy
+        z = depth_map
+
+        ones = np.ones_like(z)
+        ones = np.where(z == 0, 0, ones)
+
+        points_cam = np.stack([x, y, z, ones], axis=-1)
+        points_cam = points_cam.reshape(-1, 4)
+
+        points_sfm = points_cam @ T_sfm_cam.T
+
+        points_sfm = points_sfm[:, :3]
+
+        points_sfm = points_sfm.reshape(depth_map.shape[0], depth_map.shape[1], 3)
+
+        assert points_sfm.shape == depth_map.shape + (3,), points_sfm.shape
+
+        return points_sfm
+    
+    @staticmethod
+    def visualize_scene_coordinate_map(
+            coordinates: np.ndarray,
+            path_to_output: Path = None,
+        ):
+        """
+        Visualize scene coordinates.
+        """
+
+        cmap = plt.get_cmap('viridis')
+
+        mask = np.all(coordinates == [0., 0., 0.], axis=-1)
+        masked_coordinates = np.ma.masked_array(coordinates, mask=np.repeat(mask[:, :, np.newaxis], 3, axis=2))
+        
+        # Normalize the coordinates to the range [0, 1]
+        normalized_coordinates = (masked_coordinates - masked_coordinates.min(axis=(0, 1))) / (masked_coordinates.max(axis=(0, 1)) - masked_coordinates.min(axis=(0, 1)))
+
+        # set all masked values to white
+        normalized_coordinates = np.where(mask[:, :, np.newaxis], 1, normalized_coordinates)
+        
+        fig, ax = plt.subplots(figsize=(10, 8))
+
+        # Use the normalized coordinates as RGB values
+        im = ax.imshow(normalized_coordinates, cmap=cmap)
+        
+        # ax.set_title('3D Coordinate Visualization')
+
+        # ax.set_xlabel('X')
+        # ax.set_ylabel('Y')
+        # ax.axis('off')
+
+        ax.set_xticks([])
+        ax.set_yticks([])
+        
+        if not path_to_output:
+            plt.show()
+        else:
+            plt.savefig(path_to_output / f'{name}_coordinates.png', transparent=True)
+        plt.close()
+        
+
+
+if __name__ == '__main__':
+
+    # path_to_database = Path('/Users/eric/Documents/Studies/MSc Robotics/Thesis/Evaluation/notre_dame_B/inputs/database/')
+
+    # path_to_depth = path_to_database / 'depth/'
+    # path_to_poses = path_to_database / 'poses/'
+
+
+    # names = ['d110_h135_z20', 'd160_h175_z20', 'd110_h125_z2']
+
+    # path_to_visualization = Path('/Users/eric/Documents/Studies/MSc Robotics/Thesis/Visualization/')
+
+    # for name in names:
+
+    #     ModelConversion.visualize_depth_map(path_to_depth, name, path_to_visualization)
+
+        # depth_name = name.split('.')[0]
+        # if not depth_name.endswith('_depth'):
+        #     depth_name += '_depth'
+        # depth_name = depth_name + '.npz'
+        # depth_map = np.load(path_to_depth / depth_name)['depth']
+
+        # camera_intrinsics = [995.5555555555555, 995.5555555555555, 512.0, 512.0]
+
+        # pose_name = name.split('.')[0] + '.txt'
+        # pose_cam_sfm = np.loadtxt(path_to_poses / pose_name)
+
+
+        # scene_coordinates = ModelConversion.convert_depth_map_to_scene_coordinate_map(depth_map, camera_intrinsics, pose_cam_sfm)
+
+        # ModelConversion.visualize_scene_coordinate_map(scene_coordinates, path_to_visualization)
+
+    
+    path_to_ground_truth = Path('/Users/eric/Documents/Studies/MSc Robotics/Thesis/Evaluation/notre_dame_B/ground_truth/')
+    path_to_depth = path_to_ground_truth / 'renders/depth/'
+
+    ModelConversion.visualize_depth_map(path_to_depth, 'query_00870470_3859452456_depth.npz')
+
+
+
+    # path_to_ground_truth = Path('/Users/eric/Documents/Studies/MSc Robotics/Thesis/Evaluation/notre_dame_E/ground_truth/')
+    # path_to_database = Path('/Users/eric/Documents/Studies/MSc Robotics/Thesis/Evaluation/notre_dame_E/inputs/database/')
+
+    # model_conversion = ModelConversion(path_to_ground_truth, path_to_database)
+    # model_conversion.convert_render_intrinsics_and_poses_to_colmap_format(from_blender_format=True)
+    # model_conversion.convert_depth_maps_from_exr_to_npz()
