031878fb15a007d2457414faaf9fe1ca6f2fae64
etueschenboe <eric.tueschenboenner@gmail.com>
2024-08-25 12:47:13 +0200

    Efficient rendering of multiple models

--- src/blender/render_database.py
+++ src/blender/render_database.py
@@ -4,52 +4,46 @@ import os
 script_directory = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(script_directory)
 
-
 from render import Blender
-from render_data import blender_dir, models
+
+model_dir = '/Users/eric/Documents/Studies/MSc Robotics/Thesis/3D Models'
+renders_dir = '/Users/eric/Library/Mobile Documents/com~apple~CloudDocs/Blender/renders'
+
+# notre dame B: {model_dir}/Notre Dame/B/notre dame B.blend
+# notre dame E
+# reichstag A
+# reichstag B
+# reichstag E
+# st peters square B:  /St Peters Square/B/st peters square B.blend
+# st peters square C: /St Peters Square/C/st peters square C.blend
+
+models = {
+    'Notre Dame': ['B'], #, 'E'],
+    'Reichstag': ['A', 'B'], #, 'E'],
+    # 'St Peters Square': ['B', 'C']
+}
 
 
-if __name__ == "__main__":
+for name in models:
+    for id in models[name]:
 
-    for model in ['notre dame B']:
+        print(f'Model: {name} / {id}')
+        full_name = (name.lower() + ' ' + id) # e.g. notre dame B
 
-        blend_file = models[model]['blend_file']
-        target_name = models[model]['target_name']
+        blend_file = f'{model_dir}/{name}/{id}/{full_name}.blend'
+        assert os.path.exists(blend_file), f"Blend file not found: {blend_file}"
 
-        render_dir = f'{blender_dir}renders/{model}/'
-
+        render_dir = f'{renders_dir}/{full_name}/'
+        
         blender = Blender(
             blend_file,
             render_dir,
-            target_name,
             )
-
-        # blender.render_orbit_views(
-        #     h_steps = 36,
-        #     v_angles_deg = [-10, 0, 10, 20]
-        #     )
         
-        # blender.render_ground_views(
-        #     distances=[110, 160],
-        #     h_steps = 72,
-        #     heights = [2, 20]
-        #     )
-
-        # blender.render_ground_views(
-        #     distances=[110, 160, 210],
-        #     h_steps = 8,
-        #     heights = [2, 20],
-        #     focal_lengths=[35, 50, 70]
-        #     )
-
-
-
-        # MULTIPLE FOCAL LENGTHS
-
         for f, d in [(30, 110), (60, 220), (120, 440)]:
             blender.render_ground_views(
                 distances=[d],
                 h_steps = 24,
                 heights = [2, 20],
-                focal_lengths=[f]
+                focal_lengths=[f],
                 )
