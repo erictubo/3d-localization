31e17a3591c944d15cc2d8d1878606073b04a147
Eric Tüschenbönner <eric.tueschenboenner@gmail.com>
2024-08-30 17:43:15 +0200

    Untested: Changed method to differentiate between renders and sfm_data rather than database and query

--- src/glace_conversion.py
+++ src/glace_conversion.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 from typing import List
 import numpy as np
+from transformations import decompose_matrix, compose_matrix
 
 from colmap_model import ColmapModelReader
-from model_conversion import convert_pose_to_matrix
+from model_conversion import *
 
 
 class GlaceConversion:
@@ -17,20 +18,19 @@ class GlaceConversion:
             self,
             path_to_input: Path,
             path_to_output: Path,
-            database: bool,
-            query: bool,
-            query_names: list = None, # list of query, else uses images in input/query/images/*
+            renders: bool,
+            sfm_data: bool,
+            sfm_names: list = None, # list of sfm names, else uses images in input/{renders_prefix}/images/*
             path_to_colmap_model: Path = None,
-            database_prefix: str = 'database/',
-            query_prefix: str = 'query/',
+            T_sfm_cad: np.ndarray = None,
+            renders_prefix: str = 'database/',
+            sfm_prefix: str = 'query/',
             train_prefix: str = 'train/',
             test_prefix: str = 'test/',
         ):
 
         self.path_to_input = path_to_input
-        self.path_to_input_database = path_to_input / database_prefix
-        if query:
-            self.path_to_input_query = path_to_input / query_prefix
+        self.path_to_input_renders = path_to_input / renders_prefix
 
         self.path_to_output = path_to_output
         self.path_to_output_train = path_to_output / train_prefix
@@ -39,34 +39,43 @@ class GlaceConversion:
         if not self.path_to_output_train.exists(): self.path_to_output_train.mkdir()
         if not self.path_to_output_test.exists(): self.path_to_output_test.mkdir()
 
-        if database:
-            self.copy_database_images()
-            self.convert_database_intrinsics()
-            self.convert_database_poses()
+        if renders:
+            self.copy_rendered_images()
+            self.convert_rendered_intrinsics()
+            self.convert_rendered_poses()
         
-        if query:
-            assert path_to_colmap_model, "path_to_colmap_model must be provided for query conversion"
+        if sfm_data:
+            self.path_to_input_sfm = path_to_input / sfm_prefix
+            assert path_to_colmap_model, "path_to_colmap_model must be provided for sfm data conversion"
             self.path_to_colmap_model = path_to_colmap_model
+
+            assert T_sfm_cad.shape == (4,4), "Reference transformation is not a 4x4 matrix"
+            self.T_sfm_cad = T_sfm_cad
+            self.T_cad_sfm = np.linalg.inv(self.T_sfm_cad)
+            
             self.colmap_model = ColmapModelReader(self.path_to_colmap_model)
 
-            if not query_names:
-                query_names = [image.name for image in self.path_to_input_query.glob('images/*')]
-            query_names = [name.split('.')[0] for name in query_names]
+            if not sfm_names:
+                sfm_names = [image.name for image in self.path_to_input_query.glob('images/*')]
+            sfm_names = [name.split('.')[0] for name in sfm_names]
 
-            self.copy_query_images(query_names)
-            self.convert_query_intrinsics(query_names)
-            self.convert_query_poses(query_names)
+            self.copy_sfm_images(sfm_names)
+            self.convert_sfm_intrinsics(sfm_names)
+            self.convert_sfm_poses(sfm_names)
+
+        # TODO: test new method for (1) renders and (2) sfm_data
         
         # TODO: implement scene coordinates
+        # Make sure they are in the correct frame as well (CAD)
         # self.copy_scene_coordinates()
 
 
-    def copy_database_images(self):
+    def copy_rendered_images(self):
         """
         Copy images from input/database/images/* to train/rgb/*
         """
 
-        database_images = list(self.path_to_input_database.glob('images/*'))
+        database_images = list(self.path_to_input_renders.glob('images/*'))
         self.num_database_images = len(database_images)
         print(f"Number of database images: {self.num_database_images}")
 
@@ -80,12 +89,12 @@ class GlaceConversion:
         print("Database images copied successfully")
 
 
-    def convert_database_intrinsics(self):
+    def convert_rendered_intrinsics(self):
         """
         Convert database/intrinsics/*.txt (w, h, f, f_unit, cx, cy) to train/calibration/*.txt (camera matrix) per image.
         """
         
-        database_intrinsics = list(self.path_to_input_database.glob('intrinsics/*.txt'))
+        database_intrinsics = list(self.path_to_input_renders.glob('intrinsics/*.txt'))
         self.num_database_intrinsics = len(database_intrinsics)
 
         assert self.num_database_intrinsics == self.num_database_images, \
@@ -117,12 +126,13 @@ class GlaceConversion:
         print("Database intrinsics converted successfully")
 
 
-    def convert_database_poses(self):
+    def convert_rendered_poses(self):
         """
         Convert database/poses/*.txt (px, py, pz, qw, qx, qy, qz) to poses/*.txt (transformation matrix) per image
+        Format: CAD (inverted Blender camera) -> CAD (conventional camera)
         """
 
-        database_poses = list(self.path_to_input_database.glob('poses/*.txt'))
+        database_poses = list(self.path_to_input_renders.glob('poses/*.txt'))
         self.num_database_poses = len(database_poses)
 
         assert self.num_database_poses == self.num_database_images, \
@@ -132,16 +142,18 @@ class GlaceConversion:
         if not path_to_output_poses.exists(): path_to_output_poses.mkdir()
 
         for file in database_poses:
-            pose = np.loadtxt(file)
-            pose_matrix = convert_pose_to_matrix(pose)
+            pose_cad_cam_blender = np.loadtxt(file)
+            T_cad_cam_blender = convert_pose_to_matrix(pose_cad_cam_blender)
+
+            T_cad_cam = reverse_camera_pose_for_blender(T_cad_cam_blender, frame='CAD')
 
             output_file = path_to_output_poses / file.name
-            np.savetxt(output_file, pose_matrix, fmt='%15.7e')
+            np.savetxt(output_file, T_cad_cam, fmt='%15.7e')
         
         print("Database poses converted successfully")
 
 
-    def copy_query_images(self, query_names: List[str]):
+    def copy_sfm_images(self, sfm_names: List[str]):
         """
         Copy images from path_to_colmap_model/../images/* to test/rgb/*
         """
@@ -150,7 +162,7 @@ class GlaceConversion:
         if not path_to_output_rgb.exists(): path_to_output_rgb.mkdir()
 
         path_to_colmap_images = self.path_to_colmap_model.parent / 'images/'
-        for name in query_names:
+        for name in sfm_names:
             image = path_to_colmap_images / f'{name}.jpg'
             output_image = path_to_output_rgb / f'{name}.jpg'
             output_image.write_bytes(image.read_bytes())
@@ -159,7 +171,7 @@ class GlaceConversion:
         
     
 
-    def convert_query_intrinsics(self, query_names: List[str]):
+    def convert_sfm_intrinsics(self, sfm_names: List[str]):
         """
         Convert query intrinsics from colmap_model to train/calibration/*.txt (camera matrix) per image
         """
@@ -167,7 +179,7 @@ class GlaceConversion:
         path_to_output_calibration = self.path_to_output_test / 'calibration/'
         if not path_to_output_calibration.exists(): path_to_output_calibration.mkdir()
 
-        for name in query_names:
+        for name in sfm_names:
             id = self.colmap_model.get_query_image_id(f'{name}.jpg')
             camera_model, w, h, camera_params = self.colmap_model.get_query_intrinsics(id)
 
@@ -188,7 +200,7 @@ class GlaceConversion:
         print("Query intrinsics converted successfully")
 
 
-    def convert_query_poses(self, query_names: List[str]):
+    def convert_sfm_poses(self, sfm_names: List[str]):
         """
         Convert query poses from colmap_model to test/poses/*.txt (transformation matrix) per image
         """
@@ -196,14 +208,17 @@ class GlaceConversion:
         path_to_output_poses = self.path_to_output_test / 'poses/'
         if not path_to_output_poses.exists(): path_to_output_poses.mkdir()
 
-        for name in query_names:
+        for name in sfm_names:
             id = self.colmap_model.get_query_image_id(f'{name}.jpg')
-            pose = self.colmap_model.get_query_pose(id)
+            pose_cam_sfm = self.colmap_model.get_query_pose(id)
 
-            pose_matrix = convert_pose_to_matrix(pose)
+            T_cam_sfm = convert_pose_to_matrix(pose_cam_sfm)
+            T_sfm_cam = np.linalg.inv(T_cam_sfm)
+
+            T_cad_cam = self.T_cad_sfm @ T_sfm_cam
 
             output_file = path_to_output_poses / f'{name}.txt'
-            np.savetxt(output_file, pose_matrix, fmt='%15.7e')
+            np.savetxt(output_file, T_cad_cam, fmt='%15.7e')
         
         print("Query poses converted successfully")
     
@@ -212,54 +227,93 @@ class GlaceConversion:
         pass
 
 
-class ScaleConversion:
+# class FrameConversion:
 
-    def __init__(
-            self,
-            scale: float,
-            path_to_input: Path,
-            path_to_output: Path,
-            type: str = 'matrix',
-        ):
-        self.scale = scale
-        self.path_to_input = path_to_input
-        self.path_to_output = path_to_output
+#     def __init__(
+#             self,
+#             T_sfm_cad: float,
+#             path_to_input: Path,
+#             path_to_output: Path,
+#             type: str = 'matrix',
+#         ):
+#         self.T_sfm_cad = T_sfm_cad
+#         self.T_cad_sfm = np.linalg.inv(T_sfm_cad)
+
+#         self.scale, shear, angles, translate, perspective = decompose_matrix(self.T_cad_sfm)
+#         print('Scale: ', self.scale)
+
+#         self.path_to_input = path_to_input
+#         self.path_to_output = path_to_output
         
-        if not self.path_to_input.exists():
-            raise FileNotFoundError(f"{self.path_to_input} does not exist")
-        if not self.path_to_output.exists(): self.path_to_output.mkdir()
+#         if not self.path_to_input.exists():
+#             raise FileNotFoundError(f"{self.path_to_input} does not exist")
+#         if not self.path_to_output.exists(): self.path_to_output.mkdir()
 
-        if type != 'matrix':
-            raise NotImplementedError(f"Type {type} not implemented")
+#         if type != 'matrix':
+#             raise NotImplementedError(f"Type {type} not implemented")
+        
+#     def reverse_camera_direction(self):
+#         for file in self.path_to_input.glob('*.txt'):
+#             T = np.loadtxt(file)
+#             assert T.shape == (4, 4), f"Pose matrix shape {T.shape} is not 4x4"
+#             T_rev = reverse_camera_pose_for_blender(T, frame='CAD')
+#             output_file = self.path_to_output / file.name
+#             np.savetxt(output_file, T_rev, fmt='%15.7e')
 
-        self.scale_poses()
+#     def convert_cad_to_sfm_frame(self, reverse_camera):
+#         """
+#         Convert pose matrices using conversion matrix.
+#         """
+        
+#         for file in self.path_to_input.glob('*.txt'):
+#             T = np.loadtxt(file)
+#             assert T.shape == (4, 4), f"Pose matrix shape {T.shape} is not 4x4"
 
-    def scale_poses(self):
-        """
-        Scale pose matrices by self.scale and save to path
-        """
+#             if reverse_camera:
+#                 T = reverse_camera_pose_for_blender(T, frame='SFM')
+#             T = self.T_sfm_cad @ T
 
-        for file in self.path_to_input.glob('*.txt'):
-            pose = np.loadtxt(file)
-            assert pose.shape == (4, 4), f"Pose matrix shape {pose.shape} is not 4x4"
-            pose[:3, 3] *= self.scale
+#             output_file = self.path_to_output / file.name
+#             np.savetxt(output_file, T, fmt='%15.7e')
 
-            output_file = self.path_to_output / file.name
-            np.savetxt(output_file, pose, fmt='%15.7e')
+
+#     def scale_poses(self, up=True):
+#         """
+#         Scale pose matrices by self.scale and save to path
+#         """
+
+#         for file in self.path_to_input.glob('*.txt'):
+#             T = np.loadtxt(file)
+#             assert T.shape == (4, 4), f"Pose matrix shape {T.shape} is not 4x4"
+#             if up:
+#                 T[:3, 3] *= self.scale
+#             else:
+#                 T[:3, 3] /= self.scale
+
+#             output_file = self.path_to_output / file.name
+#             np.savetxt(output_file, T, fmt='%15.7e')
+
 
 
 if __name__ == '__main__':
 
-    scale_notre_dame = 11.718009811983837
-    scale_reichstag  = 15.304415226012821
-    scale_st_peters  = 21.77207755406891
-    
+    T_notre_dame = np.array([
+        [-0.04308, -0.07366, -0.0008805, -1.525],
+        [0.0245, -0.01336, -0.08065, 4.145],
+        [0.06947, -0.04097, 0.02789, 10.74],
+        [0, 0, 0, 1]
+    ])
 
-    ScaleConversion(
-        scale=11.718,
-        path_to_input=Path('/Users/eric/Developer/glace/datasets/notre_dame_B/train/poses/'),
-        path_to_output=Path('/Users/eric/Developer/glace/datasets/notre_dame_B/train/scaled_poses/'),
-    )
+    path_to_data = Path('/home/johndoe/Documents/data/GLACE/')
+
+    # for name in ['notre dame B (real)', 'notre dame B (SFM renders)']:
+    #     for split in ['train', 'test']:
+    
+    #         FrameConversion(
+    #             T_sfm_cad=T_notre_dame,
+    #             path_to_input=path_to_data / name / split / 'poses CAD Blender',
+    #             path_to_output=path_to_data / name / split / 'poses SFM rev',
+    #         ).convert_cad_to_sfm_frame(reverse_camera=True)
 
 
 
@@ -267,9 +321,9 @@ if __name__ == '__main__':
     # GlaceConversion(
     #     path_to_input=Path('/Users/eric/Documents/Studies/MSc Robotics/Thesis/Evaluation/notre_dame_B_new/inputs/'),
     #     path_to_output=Path('/Users/eric/Developer/glace/datasets/notre dame B/'),
-    #     database=True,
+    #     renders=True,
     #     query=True,
-    #     query_names=[
+    #     sfm_names=[
     #             '00145492_2294287255.jpg',
     #             '00870470_3859452456.jpg',
     #             '01133173_279144982.jpg',
@@ -282,24 +336,22 @@ if __name__ == '__main__':
     # )
 
 
-    # Notre Dame B with orbit renders
+    # Notre Dame B: SFM poses
+    GlaceConversion(
+        path_to_input=Path('/home/johndoe/Documents/data/Evaluation/st peters square B/ground truth/'),
+        path_to_output=Path('/home/johndoe/Documents/data/GLACE/st peters square B (SFM renders)/'),
+        renders=True,
+        renders_prefix='renders/',
+        sfm_data=False,
+    )
+
+
+    # # Notre Dame B with orbit renders
     # GlaceConversion(
     #     path_to_input=Path('/home/johndoe/Documents/data/Evaluation/notre dame B/inputs/'),
-    #     path_to_output=Path('/home/johndoe/Documents/data/GLACE/notre dame B rendered/'),
-    #     database=True,
-    #     query=True,
-    #     query_names=None,
-    #     path_to_colmap_model=Path('/home/johndoe/Documents/data/3D Models/Notre Dame/Reference/dense/sparse')
-    # )
-
-    # Notre Dame B with real images as training
-
-
-    # Notre Dame B with rendered SFM poses
-    # GlaceConversion(
-    #     path_to_input=Path('/home/johndoe/Documents/data/Evaluation/notre dame B/ground truth/'),
-    #     path_to_output=Path('/home/johndoe/Documents/data/GLACE/notre dame B rendered/'),
-    #     database_prefix='renders/',
-    #     database=True,
-    #     query=False,
+    #     path_to_output=Path('/home/johndoe/Documents/data/GLACE/notre dame B (orbit renders)/'),
+    #     renders=True,
+    #     sfm_data=False,
+    #     # sfm_names=None,
+    #     # path_to_colmap_model=Path('/home/johndoe/Documents/data/3D Models/Notre Dame/Reference/dense/sparse')
     # )
