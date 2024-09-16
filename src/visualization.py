82f42f690a346a0205e3c1d7abca573059e6cff1
etueschenboe <eric.tueschenboenner@gmail.com>
2024-09-16 19:21:55 +0200

    Added comparison for scene coordinates and depth maps

--- src/visualization.py
+++ src/visualization.py
@@ -258,29 +258,215 @@ class Visualization:
             plt.savefig(path_to_output / f'{name}_coordinates.png', transparent=True)
         plt.close()
 
+    
+    def compare_scene_coordinate_maps(
+            path_to_scene_coordinates_1: Path,
+            path_to_scene_coordinates_2: Path,
+            path_to_output: Path = None,
+        ):
+        """
+        Visualize the pixel-wise difference between two scene coordinate maps.
+        Difference = distance between the two points in 3D space
+        """
+        
+        def load_coordinates(path_to_scene_coordinates: Path):
+            format = path_to_scene_coordinates.suffix[1:]
+            if format == 'dat':
+                import torch
+                coordinates = torch.load(path_to_scene_coordinates)
+                # Torch tensor (3, H, W) -> numpy array (H, W, 3)
+                coordinates = coordinates.permute(1, 2, 0).numpy()
+            else:
+                coordinates = np.load(path_to_scene_coordinates)['scene_coordinates']
+            mask = np.all(coordinates == [0., 0., 0.], axis=-1)
+            return coordinates, mask
+        
+        coordinates_1, mask_1 = load_coordinates(path_to_scene_coordinates_1)
+        coordinates_2, mask_2 = load_coordinates(path_to_scene_coordinates_2)
+
+        # combine the masks: multiply
+        mask = mask_1 | mask_2
+
+        # Calculate the difference
+        difference = np.linalg.norm(coordinates_1 - coordinates_2, axis=-1)
+        masked_difference = np.ma.masked_array(difference, mask=mask)
+
+        print(difference.shape)
+
+        cmap = plt.get_cmap('viridis')
+        cmap.set_bad(color='white')
+        
+        fig, ax = plt.subplots(figsize=(10, 8))
+
+        # Create a color-coded image of the depth map
+        im = ax.imshow(masked_difference, cmap=cmap)
+
+        # Reverse the y-axis
+        # ax.invert_yaxis()
+        
+        # Add a colorbar
+        cbar = fig.colorbar(im, ax=ax)
+        cbar.set_label('Distance', rotation=270, labelpad=15)
+        
+        # ax.set_title('Depth Map Visualization')
+
+        # ax.set_xlabel('X')
+        # ax.set_ylabel('Y')
+        # ax.axis('off')
+
+        ax.set_xticks([])
+        ax.set_yticks([])
+        
+        if not path_to_output:
+            plt.show()
+        else:
+            plt.savefig(path_to_output / f'{name}.png', transparent=True)
+        plt.close()
+
+    
+    def compare_depth_maps(
+            path_to_depth_1: Path,
+            path_to_depth_2: Path,
+            path_to_output: Path = None,
+    ):
+        """
+        Visualize the pixel-wise difference between two depth maps.
+        Difference = absolute difference between the two depth values
+        """
+
+        def load_depth_map(path_to_depth: Path):
+            format = path_to_depth.suffix[1:]
+            if format == 'npz':
+                depth_map = np.load(path_to_depth)['depth']
+            elif format == 'npy':
+                depth_map = np.load(path_to_depth)
+            mask = depth_map == 0.
+            return depth_map, mask
+
+        depth_map_1, mask_1 = load_depth_map(path_to_depth_1)
+        depth_map_2, mask_2 = load_depth_map(path_to_depth_2)
+
+        mask = mask_1 | mask_2
+
+        # Calculate the difference
+        difference = np.abs(depth_map_1 - depth_map_2)
+        masked_difference = np.ma.masked_array(difference, mask=mask)
+
+        cmap = plt.get_cmap('viridis')
+        cmap.set_bad(color='white')
+        
+        fig, ax = plt.subplots(figsize=(10, 8))
+
+        # Create a color-coded image of the depth map
+        im = ax.imshow(masked_difference, cmap=cmap)
+
+        # Reverse the y-axis
+        # ax.invert_yaxis()
+        
+        # Add a colorbar
+        cbar = fig.colorbar(im, ax=ax)
+        cbar.set_label('Difference', rotation=270, labelpad=15)
+        
+        # ax.set_title('Depth Map Visualization')
+
+        # ax.set_xlabel('X')
+        # ax.set_ylabel('Y')
+        # ax.axis('off')
+
+        ax.set_xticks([])
+        ax.set_yticks([])
+        
+        if not path_to_output:
+            plt.show()
+        else:
+            plt.savefig(path_to_output / f'{name}.png', transparent=True)
+        plt.close()
+        
+
+
 
 if __name__ == '__main__':
 
-    path_to_data = Path('/Users/eric/Documents/Studies/MSc Robotics/Thesis/Data/')
+    from data import path_to_data
 
-    path_to_query_images = path_to_data / 'Evaluation/notre dame B/inputs/query/images'
-    path_to_render_images = path_to_data / 'Evaluation/notre dame B/ground truth/renders/images'
-    path_to_overlays = path_to_data / 'Evaluation/notre dame B/ground truth/overlays'
-    path_to_overlays_subsampled = path_to_data / 'Evaluation/notre dame B/ground truth/overlays/subsampled'
+    # path_to_query_images = path_to_data / 'Evaluation/notre dame B/inputs/query/images'
+    # path_to_render_images = path_to_data / 'Evaluation/notre dame B/ground truth/renders/images'
+    # path_to_overlays = path_to_data / 'Evaluation/notre dame B/ground truth/overlays'
+    # path_to_overlays_subsampled = path_to_data / 'Evaluation/notre dame B/ground truth/overlays/subsampled'
 
-    Visualization.overlay_query_and_rendered_images(path_to_query_images, path_to_render_images, path_to_overlays, subsample=1)
-    Visualization.overlay_query_and_rendered_images(path_to_query_images, path_to_render_images, path_to_overlays_subsampled, subsample=8)
-    Visualization.overlay_query_and_rendered_images(path_to_query_images, path_to_render_images, subsample=8)
+    # Visualization.overlay_query_and_rendered_images(path_to_query_images, path_to_render_images, path_to_overlays, subsample=1)
+    # Visualization.overlay_query_and_rendered_images(path_to_query_images, path_to_render_images, path_to_overlays_subsampled, subsample=8)
+    # Visualization.overlay_query_and_rendered_images(path_to_query_images, path_to_render_images, subsample=8)
 
-    # path_to_scene_coordinates = Path('/Users/eric/Documents/Studies/MSc Robotics/Thesis/Data/Evaluation/notre dame B/inputs/database/scene coordinates/')
+
+
+    # path_to_scene_coordinates = path_to_data / 'Evaluation/notre dame B/inputs/database/scene coordinates/'
 
     # names = ['f30_d110_z20_h195.npz'] #, 'f30_d110_z20_h210.npz', 'f30_d110_z20_h225.npz', 'f30_d110_z20_h240.npz', 'f30_d110_z20_h255.npz']
 
     # for name in names:
     #     Visualization.visualize_scene_coordinate_map(path_to_scene_coordinates, name)
 
-    # path_to_scene_coordinates = Path('/Users/eric/Documents/Studies/MSc Robotics/Thesis/Data/GLACE/notre dame (SFM)/train/init/')
-    # path_to_depth = Path('/Users/eric/Documents/Studies/MSc Robotics/Thesis/Data/GLACE/notre dame (SFM)/train/depth/')
+
+
+    path_to_scene_coordinates = path_to_data / 'GLACE/pantheon (renders)/test/init/'
+    path_to_depth = path_to_data / 'GLACE/pantheon (renders)/test/depth/'
+    format = 'dat'
+
+    names = ['query_00318896_2265892479']
+
+    for name in names:
+        Visualization.visualize_depth_map(
+            path_to_depth=path_to_depth,
+            name=name,
+            extension='npy',
+        )
+        # Visualization.visualize_scene_coordinate_map(
+        #     path_to_scene_coordinates,
+        #     name,
+        #     format=format,
+        #     # path_to_output=path_to_scene_coordinates,
+        #     # x_range=(-120, 100),
+        #     # y_range=(-80, 80),
+        #     # z_range=(-20, 120),
+        # )
+
+    path_to_scene_coordinates = path_to_data / 'GLACE/pantheon (SFM)/test/init/'
+    path_to_depth = path_to_data / 'GLACE/pantheon (SFM)/test/depth/'
+    format = 'dat'
+
+    names = ['00318896_2265892479']
+
+    for name in names:
+        Visualization.visualize_depth_map(
+            path_to_depth=path_to_depth,
+            name=name,
+            extension='npy',
+        )
+        # Visualization.visualize_scene_coordinate_map(
+        #     path_to_scene_coordinates,
+        #     name,
+        #     format=format,
+        #     # path_to_output=path_to_scene_coordinates,
+        #     # x_range=(-120, 100),
+        #     # y_range=(-80, 80),
+        #     # z_range=(-20, 120),
+        # )
+
+    # Visualization.visualize_scene_coordinate_difference(
+    #     path_to_scene_coordinates_1=path_to_data / 'GLACE/pantheon (renders)/test/init/query_00318896_2265892479.dat',
+    #     path_to_scene_coordinates_2=path_to_data / 'GLACE/pantheon (SFM)/test/init/00318896_2265892479.dat',
+    # )
+
+    Visualization.compare_depth_maps(
+        path_to_depth_1=path_to_data / 'GLACE/pantheon (renders)/test/depth/query_00318896_2265892479.npy',
+        path_to_depth_2=path_to_data / 'GLACE/pantheon (SFM)/test/depth/00318896_2265892479.npy',
+    )
+
+
+
+    # path_to_scene_coordinates = path_to_data / 'GLACE/notre dame (SFM)/train/init/'
+    # path_to_depth = path_to_data / 'GLACE/notre dame (SFM)/train/depth/'
     # format = 'dat'
 
     # # names = ['49379137_4824496602', '49452387_8136855930', '49610648_2419143510']
